fc131cb217198f09e6ec55e3d474901ea8e1a385
Jay-Karia <jay.sanjay.karia@gmail.com>
2023-11-05 23:34:36 +0530

    todo app done

--- Video/next01/app/components/TodoItem.tsx
+++ Video/next01/app/components/TodoItem.tsx
@@ -1,16 +1,23 @@
+"use client"
 import React from 'react';
+import Link from "next/link"
 
 type TodoItemProps = {
     id: string,
     title: string,
-    complete: boolean
+    complete: boolean,
+    toggleTodo: (id: string, complete: boolean) => void
+    deleteTodo: (id: string) => void
 }
 
-function TodoItem({id, title, complete}: TodoItemProps) {
+function TodoItem({id, title, complete, toggleTodo, deleteTodo}: TodoItemProps) {
     return (
-        <li className={"flex gap-1 items-center"}>
-            <input id={id} type={"checkbox"} className={"form-checkbox h-5 w-5 text-slate-600 cursor-pointer peer"} />
-            <label htmlFor={id} className={"text-slate-100 cursor-pointer peer-checked:line-through peer-checked:text-slate-500"}>{title}</label>
+        <li className={"flex gap-4 items-center"}>
+            <input id={id} type={"checkbox"} className={"form-checkbox h-5 w-5 text-slate-600 cursor-pointer peer"}
+                   defaultChecked={complete} onChange={e => toggleTodo(id, e.target.checked)}/>
+            <label htmlFor={id}
+                   className={"text-slate-100 cursor-pointer peer-checked:line-through peer-checked:text-slate-500"}>{title}</label>
+            <button className={"cursor-pointer"} onClick={()=>{deleteTodo(id)}}>delete</button>
         </li>
     );
 }
--- Video/next01/app/page.tsx
+++ Video/next01/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
-import {PrismaClient } from "@prisma/client"
+import {PrismaClient} from "@prisma/client"
 import TodoItem from "@/app/components/TodoItem";
+import {redirect} from "next/navigation";
 
 interface Todo {
     title: string,
@@ -10,20 +11,45 @@ interface Todo {
     updatedAt: Date
 }
 
+async function toggleTodo(id: string, complete: boolean) {
+    "use server"
+
+    const prisma = new PrismaClient()
+    await prisma.todo.update({
+        where: {
+            id
+        },
+        data: {
+            complete
+        }
+    } as any)
+
+}
+
+async function deleteTodo(id: string) {
+    "use server"
+
+    const prisma = new PrismaClient()
+    await prisma.todo.delete({where: {id}})
+
+}
+
 export default async function Home() {
     const prisma = new PrismaClient()
 
-    const todos : Todo[] = await prisma.todo.findMany()
+    const todos: Todo[] = await prisma.todo.findMany()
 
     return (
         <>
             <header className={"flex justify-between mb-4 items-center"}>
                 <h1 className={"text-2xl"}>Todos</h1>
-                <Link className={"border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"} href={"/new"}>New</Link>
+                <Link
+                    className={"border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"}
+                    href={"/new"}>New</Link>
             </header>
             <ul className={"pl-4"}>
-                {todos.map(todo=> (
-                    <TodoItem id={todo.id} title={todo.title} complete={todo.complete}/>
+                {todos.map(todo => (
+                    <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
                 ))}
             </ul>
         </>
