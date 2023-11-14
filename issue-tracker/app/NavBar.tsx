3f7e89bb0a7d46e35b23dc7e3693c7b3d9a8dbd1
Jay-Karia <jay.sanjay.karia@gmail.com>
2023-11-14 16:57:18 +0530

    Active link styling

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -1,18 +1,30 @@
+"use client"
+
 import React from 'react';
 import Link from "next/link";
 import {FaBug} from "react-icons/fa6";
+import {usePathname} from "next/navigation";
+import classnames from "classnames";
 
 function NavBar() {
+
+    const currentPath = usePathname()
+
     const links = [
         {href: '/', label: 'Dashboard'},
         {href: '/issues', label: 'Issues'},
     ]
+
     return (
         <nav className={"flex items-center space-x-6 h-14 border-b px-5"}>
             <Link href={"/"}><FaBug/></Link>
             <ul className={"flex space-x-6"}>
                 {links.map(link => (
-                    <Link className={"text-zinc-500 hover:text-zinc-800"} href={link.href} key={link.href}>
+                    <Link className={classnames({
+                        "text-zinc-900": currentPath === link.href,
+                        "text-zinc-500": currentPath !== link.href,
+                        "hover:text-zinc-800 transition-colors": true,
+                    })} href={link.href} key={link.href}>
                         {link.label}
                     </Link>
                 ))}
---
+++ issue-tracker/app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function IssuesPage() {
+    return (
+        <div>
+            Issues Page
+        </div>
+    );
+}
+
+export default IssuesPage;
