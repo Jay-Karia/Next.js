3c89c818604b02682599e52adfdbbec70c35bb36
Jay-Karia <jay.sanjay.karia@gmail.com>
2023-11-16 09:23:32 +0530

    client side validation

--- issue-tracker/app/api/issues/route.ts
+++ issue-tracker/app/api/issues/route.ts
@@ -1,20 +1,15 @@
-import {NextResponse, NextRequest} from "next/server";
-import {z} from "zod";
+import {NextRequest, NextResponse} from "next/server";
 import client from "@/prisma/client";
-
-const schema = z.object({
-    title: z.string().min(1, 'Title is required').max(255, 'Exceeded max length'),
-    description: z.string().min(1, 'Description is required').max(1000, 'Exceeded max length'),
-})
+import {createIssueSchema} from "@/app/validationSchemas";
 
 export async function POST(request: NextRequest) {
     try {
         const body = await request.json();
         const {title, description} = body;
-        const valid = schema.safeParse({title, description});
+        const valid = createIssueSchema.safeParse({title, description});
 
         if (!valid.success) {
-            return NextResponse.json({message: "Invalid data", errors: valid.error, success: false}, {status: 400});
+            return NextResponse.json({message: "Invalid data", errors: valid.error.format(), success: false}, {status: 400});
         }
 
         const newIssue = await client.issue.create({
--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -6,16 +6,18 @@ import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import {zodResolver} from "@hookform/resolvers/zod"
+import {createIssueSchema} from "@/app/validationSchemas";
+import {z} from "zod"
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 function NewIssuesPage() {
     const [error, setError] = useState("");
     const router = useRouter();
-    const { register, handleSubmit, control } = useForm<IssueForm>();
+    const { register, handleSubmit, control, formState: {errors} } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema),
+    });
 
     return (
         <div className="space-y-3">
@@ -41,6 +43,7 @@ function NewIssuesPage() {
                         {...register("title")}
                     />
                 </TextField.Root>
+                {errors.title && <p className={"text-red-700"}>{errors.title.message}</p>}
                 <Controller
                     render={({ field }) => (
                         <SimpleMDE placeholder={"Description"} {...field} />
@@ -48,6 +51,7 @@ function NewIssuesPage() {
                     name={"description"}
                     control={control}
                 />
+                {errors.description && <p className={"text-red-700"}>{errors.description.message}</p>}
                 <Button style={{ marginTop: "1rem" }}>Create New Issue</Button>
             </form>
         </div>
