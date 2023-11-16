9d28c1fe93c8932c69c9aeeaf6c2d92506f11de1
Jay-Karia <jay.sanjay.karia@gmail.com>
2023-11-16 13:06:02 +0530

    styled issue page

--- issue-tracker/app/issues/[id]/page.tsx
+++ issue-tracker/app/issues/[id]/page.tsx
@@ -1,38 +1,48 @@
-import React from 'react'
+import React from "react";
 import prisma from "@/prisma/client";
-import { notFound } from 'next/navigation';
+import { notFound } from "next/navigation";
+import { Status } from "@prisma/client";
+import StatusBadge from "@/app/components/StatusBadge";
+import { Heading, Text, Flex, Card } from "@radix-ui/themes";
 
 type Props = {
     params: {
-        id: string
-    }
-}
+        id: string;
+    };
+};
 
-async function IssueDetailPage ({params}:Props) {
-    const {id} = params;
+async function IssueDetailPage({ params }: Props) {
+    const { id } = params;
     let issue;
+
     try {
         issue = await prisma.issue.findUnique({
             where: {
-                id: Number(id)
-            }
-        })
-        console.log(issue)
-        if (!issue) notFound()
+                id: Number(id),
+            },
+        });
+
+        if (!issue) {
+            throw new Error("Issue not found"); // Throw an error to trigger the 404 page
+        }
+
+        console.log(issue);
     } catch (error) {
-        notFound()
+        notFound(); // Call notFound when an error occurs
     }
 
-    
     return (
         <div>
-            <p>{issue.title}</p>
-            <p>{issue.description}</p>
-            <p>{issue.status}</p>
-            <p>{issue.createdAt.toDateString()}</p>
-
+            <Heading weight={"bold"}>{issue.title}</Heading>
+            <Flex className={"flex space-x-3 mt-2"}>
+                <StatusBadge status={issue.status as Status} />
+                <Text weight={"light"}>{issue.createdAt.toDateString()}</Text>
+            </Flex>
+            <Card className="mt-2">
+                <Text>{issue.description}</Text>
+            </Card>
         </div>
-    )
+    );
 }
 
 export default IssueDetailPage;
