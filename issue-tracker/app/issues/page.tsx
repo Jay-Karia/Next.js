51f5b5a49f1d92ea363ec4d300f54e152d2626be
Jay-Karia <jay.sanjay.karia@gmail.com>
2023-11-25 20:14:31 +0530

    paginating issues

--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -8,21 +8,32 @@ import IssueActions from "../components/IssueActions";
 import LinkComponent from "../components/LinkComponent";
 import { Status, Issue } from "@prisma/client";
 import { ArrowUpIcon } from "@radix-ui/react-icons";
+import Pagination from "../components/Pagination";
 
 async function IssuesPage({
     searchParams,
 }: {
-    searchParams: { status: Status; orderBy: keyof Issue };
+    searchParams: { status: Status; orderBy: keyof Issue; page: string };
 }) {
     const orderBy = searchParams.orderBy
         ? { [searchParams.orderBy]: "asc" }
         : undefined;
 
+    let page = parseInt(searchParams.page);
+    const pageSize = 10;
+    page = page > 0 ? page : 1;
+
     const issues = await prisma.issue.findMany({
         where: {
             status: searchParams.status,
         },
-        orderBy
+        orderBy: orderBy,
+        skip: (page - 1) * pageSize,
+        take: pageSize,
+    });
+
+    const issueCount = await prisma.issue.count({
+        where: { status: searchParams.status },
     });
 
     const columnHeaders: {
@@ -120,6 +131,13 @@ async function IssuesPage({
                     )}
                 </Table.Body>
             </Table.Root>
+            <div className={"mt-5"}>
+                <Pagination
+                    currentPage={Number(page)}
+                    itemCount={issueCount}
+                    pageSize={pageSize}
+                />
+            </div>
         </div>
     );
 }
