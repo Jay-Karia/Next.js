184aa74aedd342e9a7ba9aed82c1e711580bae36
Jay-Karia <jay.sanjay.karia@gmail.com>
2023-11-25 17:36:26 +0530

    implementing pagination

--- issue-tracker/app/components/Pagination.tsx
+++ issue-tracker/app/components/Pagination.tsx
@@ -1,35 +1,72 @@
-import React from 'react';
-import {Flex, Text, Button} from "@radix-ui/themes"
-import {ArrowLeftIcon, ArrowRightIcon, DoubleArrowLeftIcon, DoubleArrowRightIcon} from "@radix-ui/react-icons"
+"use client"
+import React from "react";
+import { Flex, Text, Button } from "@radix-ui/themes";
+import {
+    ArrowLeftIcon,
+    ArrowRightIcon,
+    DoubleArrowLeftIcon,
+    DoubleArrowRightIcon,
+} from "@radix-ui/react-icons";
+import { useRouter, useSearchParams } from "next/navigation";
 
 type PaginationProps = {
     itemCount: number;
     pageSize: number;
     currentPage: number;
-}
+};
 
-function Pagination({itemCount, pageSize, currentPage}: PaginationProps) {
+function Pagination({ itemCount, pageSize, currentPage }: PaginationProps) {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
     const pageCount = Math.ceil(itemCount / pageSize);
     if (pageCount <= 1) return null;
 
+    const changePage = (page: number) => {
+        const params = new URLSearchParams(searchParams);
+        params.set("page", page.toString());
+        router.push("?" + params.toString());
+    };
+
     return (
         <Flex gap={"5"}>
-            <Text>Page {currentPage} of {pageCount}</Text>
+            <Text>
+                Page {currentPage} of {pageCount}
+            </Text>
             <Flex gap={"2"}>
-                <Button variant={"soft"} color={"gray"} disabled={currentPage === 1}>
+                <Button
+                    variant={"soft"}
+                    color={"gray"}
+                    disabled={currentPage === 1}
+                    onClick={() => changePage(1)}
+                >
                     <DoubleArrowLeftIcon />
                 </Button>
-                <Button variant={"soft"} color={"gray"} disabled={currentPage === 1}>
+                <Button
+                    variant={"soft"}
+                    color={"gray"}
+                    disabled={currentPage === 1}
+                    onClick={() => changePage(currentPage - 1)}
+                >
                     <ArrowLeftIcon />
                 </Button>
-                <Button variant={"soft"} color={"gray"} disabled={currentPage === pageCount}>
+                <Button
+                    variant={"soft"}
+                    color={"gray"}
+                    disabled={currentPage === pageCount}
+                    onClick={() => changePage(currentPage + 1)}
+                >
                     <ArrowRightIcon />
                 </Button>
-                <Button variant={"soft"} color={"gray"} disabled={currentPage === pageCount}>
+                <Button
+                    variant={"soft"}
+                    color={"gray"}
+                    disabled={currentPage === pageCount}
+                    onClick={() => changePage(pageCount)}
+                >
                     <DoubleArrowRightIcon />
                 </Button>
             </Flex>
-
         </Flex>
     );
 }
